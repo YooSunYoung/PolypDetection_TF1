589c110767ddd25e4f3791df3e5d106bcca2fdfd
YooSunyoug <luysunyoug9@gmail.com>
2020-10-27 15:59:57 +0100

    change the default server port number for the image feeder on CPU

--- Scripts/CPU/image_feeder_cpu.py
+++ Scripts/CPU/image_feeder_cpu.py
@@ -3,8 +3,8 @@ from Communication.ImageFeeder import ImageFeeder
 if __name__ == "__main__":
     image_feeder = ImageFeeder(port=6007,
                                server_port=6004,
-                               debug_mode=True,
-                               directory="../../data/TimeConsumption/1000/")
+                               debug_mode=False,
+                               directory="../../data/TimeConsumption/1/")
     image_feeder.build_connection()
     image_feeder.send_images()
     image_feeder.receive_results()
