4e235bd5d405999adb68279a81865d3faea294e2
YooSunyoug <luysunyoug9@gmail.com>
2021-03-02 16:05:16 +0100

    pass checkpoint path by argument

--- Training/train.py
+++ Training/train.py
@@ -22,12 +22,17 @@ class Trainer:
         self.epoch = kwargs.get("epoch", 100)
         self.save_point = kwargs.get("save_point", 50)
         self.validation_point = kwargs.get("validation_point", None)
+        self.batch_size = kwargs.get('batch_size', 32)
+        self.val_batch_size = kwargs.get('val_batch_size', 32)
         self.learning_rate = kwargs.get("learning_rate", 1e-3)
+        self.checkpoint_dir_path = kwargs.get("checkpoint_dir_path", "results/checkpoint/")
         self.device = kwargs.get("device", "/device:XLA_GPU:0")
         self.model = PolypDetectionModel()
 
     def train(self):
         train_size = len(self.train_dataset[0])
+        self.batch_size = train_size if train_size > self.batch_size else self.batch_size
+        self.val_batch_size = train_size if train_size > self.val_batch_size else self.val_batch_size
         batch_x, batch_y = None, None
         cur_loss, cur_val_loss = None, None
         lowest_loss = float('inf')
@@ -44,43 +49,42 @@ class Trainer:
                 labels = self.train_dataset[1]
                 init = tf.global_variables_initializer()
                 saver = tf.train.Saver()
-                batch_size = FLAGS.batch_size if train_size > FLAGS.batch_size else 1
-                val_batch_size = FLAGS.val_batch_size
                 with tf.Session(config=tf.ConfigProto(log_device_placement=True)) as sess:
                     writer = tf.summary.FileWriter("../summary/", sess.graph)
                     sess.run(init)
                     cur_loss = 0
                     for i in range(1, self.epoch + 1):
-                        batch_epoch_size = int(len(images) / batch_size)
+                        batch_epoch_size = int(len(images) / self.batch_size)
                         for b_num in range(batch_epoch_size):
-                            offset = b_num * batch_size
-                            if offset + batch_size < train_size:
-                                batch_x, batch_y = images[offset:(offset + batch_size)], labels[
-                                                                                     offset:(offset + batch_size)]
+                            offset = b_num * self.batch_size
+                            if offset + self.batch_size < train_size:
+                                batch_x, batch_y = images[offset:(offset + self.batch_size)], labels[
+                                                                                     offset:(offset + self.batch_size)]
                             else:
                                 batch_x, batch_y = images[offset:], labels[offset:]
                             _, cur_loss, summary = sess.run([train_op, loss, summary_op],
                                                             feed_dict={X: batch_x, Y: batch_y})
-                        print("testing loss : ", i, cur_loss)
+                        print("training loss : ", i, cur_loss)
                         if self.validate:
                             if i % self.validation_point == 0:
                                 val_size = len(self.val_dataset)
                                 val_images = self.val_dataset[0]
                                 val_labels = self.val_dataset[1]
-                                for b_num in range(int(len(val_images) / val_batch_size)):
-                                    val_offset = (b_num * val_batch_size) % (val_size - 1) if val_size > 1 else 0
-                                    val_batch_x = val_images[val_offset:(val_offset + val_batch_size)]
-                                    val_batch_y = val_labels[val_offset:(val_offset + val_batch_size)]
+                                for b_num in range(int(len(val_images) / self.val_batch_size)):
+                                    val_offset = (b_num * self.val_batch_size) % (val_size - 1) if val_size > 1 else 0
+                                    val_batch_x = val_images[val_offset:(val_offset + self.val_batch_size)]
+                                    val_batch_y = val_labels[val_offset:(val_offset + self.val_batch_size)]
                                     cur_val_loss, val_summary = sess.run([loss, summary_op],
                                                                          feed_dict={X: val_batch_x, Y: val_batch_y})
                                 if cur_val_loss < lowest_loss:
-                                    saver.save(sess, os.path.join(FLAGS.checkpoint_dir_path, "model"))
+                                    print("current lowest validation loss : ", i, cur_val_loss)
+                                    saver.save(sess, os.path.join(self.checkpoint_dir_path, "model"))
                         else:
                             if cur_loss < lowest_loss:
-                                saver.save(sess, os.path.join(FLAGS.checkpoint_dir_path, "model"))
+                                saver.save(sess, os.path.join(self.checkpoint_dir_path, "model"))
 
                         if i % self.save_point == 0 or i == self.epoch:
-                            saver.save(sess, os.path.join(FLAGS.checkpoint_dir_path, "model"), i)
+                            saver.save(sess, os.path.join(self.checkpoint_dir_path, "model"), i)
 
 
 def main(_args):
@@ -88,6 +92,8 @@ def main(_args):
     trainer = Trainer(train_image=FLAGS.train_image, train_label=FLAGS.train_label,
                       valid_image=FLAGS.valid_image, valid_label=FLAGS.valid_label, validate=FLAGS.validate,
                       epoch=FLAGS.epochs, save_point=FLAGS.save_point, validation_point=FLAGS.validation_point,
+                      batch_size=FLAGS.batch_size, val_batch_size=FLAGS.val_batch_size,
+                      checkpoint_dir_path=FLAGS.checkpoint_dir_path,
                       learning_rate=FLAGS.learning_rate)
     trainer.train()
 
