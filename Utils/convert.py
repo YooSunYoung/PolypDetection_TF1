1ba6444c91c4b547c831063c7ec9f65a399fa780
YooSunyoug <luysunyoug9@gmail.com>
2021-03-02 16:12:06 +0100

    changed default settings

--- Utils/convert.py
+++ Utils/convert.py
@@ -4,8 +4,8 @@ from Models.models import PolypDetectionModel
 from Utils.detect import get_single_image
 
 p = PolypDetectionModel()
-image = get_single_image("../data/SimpleDataSet/028.jpg")
-checkpoint_path = "../results/36_36_6_checkpoints/model-700"
+image = get_single_image("../data/PolypImages/001.jpg")
+checkpoint_path = "../results/model"
 with tf.Graph().as_default():
     X = tf.placeholder(tf.float32, [None, 227, 227, 3], name="imGRBNormalize")
     output_0 = p.get_model(X, training=False)
@@ -14,7 +14,6 @@ with tf.Graph().as_default():
     with tf.Session(config=tf.ConfigProto(log_device_placement=True)) as sess:
         sess.run(init)
         saver.restore(sess, checkpoint_path)
-        #predictions = sess.run(output_0, feed_dict={X: [image]})
         minimal_graph = convert_variables_to_constants(sess, sess.graph_def, ["FinalOutput"])
         tf.io.write_graph(minimal_graph, '.', 'model.pb', as_text=False)
         tf.io.write_graph(minimal_graph, '.', 'model.txt', as_text=True)
